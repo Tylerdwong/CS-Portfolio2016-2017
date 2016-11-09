fa53274966d2a70ee49993560d7033767b9b125f
tylerdwong <twong.sh@mysummitps.org>
2016-11-09 23:04:49 +0000

    update

--- todo.js
+++ todo.js
@@ -1,8 +1,5 @@
-var redborder = document.getElementById("redborder");
-var underline = document.getElementById("underline");
-var greenborder = document.getElementById("greenborder");
-var blueborder = document.getElementById("blueborder");
-var dropdown = document.getElementById("dropwdown");
+//Defines my variables so I can use them in my code and they are labeled.
+var dropdown = document.getElementById("dropdown");
 var submitbutton = document.getElementById("submitbutton");
 var outputarea = document.getElementById("outputarea");
 var text1 = document.getElementById("text1");
@@ -18,32 +15,37 @@ function buttonfunction() {
   outputarea.innerHTML = outputarea.innerHTML + task;
 
 
+
+
+
+
+
+
+//Tasks that make my dropdown outputs work and make what I put into my input to appear as I want it to be.
+  if (dropdown.value == "underline") {
+    outputarea.innerHTML = text1.value;
+    outputarea.style.textDecoration = "underline";
+    text1.value = "";
+
+  }
+  else if (dropdown.value == "redborder") {
+    outputarea.innerHTML = text1.value;
+    outputarea.style.borderColor = "red";
+
+  }
+  else if (dropdown.value == "greenfont") {
+    outputarea.innerHTML = text1.value;
+    outputarea.style.color = "green";
+  }
+  else if (dropdown.value == "bluefont") {
+    outputarea.innerHTML = text1.value
+    outputarea.style.color = "blue";
+
+  }
+  //"<div style = 'color'>" + color + "'>" + task + "</div>";
 }
-
-
 outputarea.addEventListener("click", function(evt) {
   var name1 = evt.target;
   name1.parentNode.removeChild(name1);
-
 });
-
-
-
-
-// if (dropdown.value == "underline") {
-//   outputarea = innerHTML = outputarea.style.textdecoration = "underline";
-
-// }
-// else if (dropdown.value == "redborder") {
-//   outputarea.innerHTML = outputarea.style.borderColor = "redborder";
-
-// }
-// else if (dropdown.value == "greenborder") {
-//   outputarea.innerHTML = outputarea.style.borderColor = "green border"
-
-// }
-// else if (dropdown.value == "blueborder") {
-//   outputarea.innerHTML = outputarea.style.borderColor = "blueborder"
-
-//}
- //"<div style = 'color'>" + color + "'>" + task + "</div>";
+//Task that deletes when I click on what I put into my textbox.  
